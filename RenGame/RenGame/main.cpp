b5d52ee113b893da1fe73fe6e36b1734310fa2a3
Josue Ccama Huallpa <josue.ccama@ucsp.edu.pe>
2014-09-06 15:21:19 -0500

    Main
    Subiendo la semilla de todo

---
+++ RenGame/RenGame/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+	cout<<"La semilla del juego"<<endl;
+	cout<<"Miau Reportandose"<<endl;
+
+	system("pause");
+	return 0;
+}
