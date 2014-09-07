4a5b70660bbcdf5e0e87c4453d48d2899fc98dbc
Josue Ccama Huallpa <josue.ccama@ucsp.edu.pe>
2014-09-07 13:07:59 -0500

    Actualizando el main
    Por ahora solo tiene una vista previa con algo basico de la compilacion
    de sfml

--- RenGame/RenGame/main.cpp
+++ RenGame/RenGame/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 int main()
@@ -7,6 +8,23 @@ int main()
 	cout<<"La semilla del juego"<<endl;
 	cout<<"Miau Reportandose"<<endl;
 
-	system("pause");
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML RenGame in Progress!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+	//system("pause");
 	return 0;
 }
